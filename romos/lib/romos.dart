3d21dc44d1d63949306ae5aae392fe5c8844a057
Klaimmor <amina.arslanova.2017@mail.ru>
2024-04-10 10:28:54 +0500

    Player mobile movement
    - Added folder with buttons images
    - Added mobile movement method
    - Added path to images in pubspec.yaml

--- romos/lib/romos.dart
+++ romos/lib/romos.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:romos/actors.dart';
 import 'package:romos/level.dart';
 import 'package:flutter/widgets.dart';
@@ -14,8 +15,6 @@ class Romos extends FlameGame with HasKeyboardHandlerComponents
   @override
   late final CameraComponent camera = CameraComponent();
   Player player = Player(character: 'Ghost');
-  @override
-  
 
   @override
   Future<void> onLoad() async
@@ -29,8 +28,68 @@ class Romos extends FlameGame with HasKeyboardHandlerComponents
 
     add(camera);
     add(world);
+    addButtons();
     
     return super.onLoad();
+  }
+  
+  void addButtons() 
+  {
+    final buttonSize = Vector2(64, 64);
+    final buttonPadding = Vector2(10, 10);
+    
+    //Button Up
+    final buttonUp = SpriteButtonComponent(
+      priority: 10,
+      button: Sprite(images.fromCache('HUD/ButtonUp.png')),
+      position: Vector2(size.x / 2 - buttonSize.x / 2, size.y - buttonSize.y * 3),
+      size: buttonSize,
+      onPressed: () 
+      {
+        player.playerDirection = PlayerDirection.up;
+      },
+    );
+
+    //Button Down
+    final buttonDown = SpriteButtonComponent(
+      priority: 10,
+      button: Sprite(images.fromCache('HUD/ButtonDown.png')),
+      position: Vector2(size.x / 2 - buttonSize.x / 2, size.y - buttonSize.y),
+      size: buttonSize,
+      onPressed: () 
+      {
+        player.playerDirection = PlayerDirection.down;
+      },
+    );
+
+    //Button Left
+    final buttonLeft = SpriteButtonComponent(
+      priority: 10,
+      button: Sprite(images.fromCache('HUD/ButtonLeft.png')),
+      position: Vector2(size.x / 2 - buttonSize.x - buttonPadding.x, size.y - buttonSize.y * 2),
+      size: buttonSize,
+      onPressed: () 
+      {
+        player.playerDirection = PlayerDirection.right;
+      },
+    );
+
+    //Button Right
+    final buttonRight = SpriteButtonComponent(
+      priority: 10,
+      button: Sprite(images.fromCache('HUD/ButtonRight.png')),
+      position: Vector2(size.x / 2 + buttonSize.x / 2 + buttonPadding.x, size.y - buttonSize.y * 2),
+      size: buttonSize,
+      onPressed: () 
+      {
+        player.playerDirection = PlayerDirection.right;
+      },
+    );
+
+    add(buttonUp);
+    add(buttonDown);
+    add(buttonLeft);
+    add(buttonRight);
   }
 
   //Camera debug
@@ -39,16 +98,16 @@ class Romos extends FlameGame with HasKeyboardHandlerComponents
   // {
   //   super.render(canvas); // Сначала рендерим все компоненты игры.
 
-  //   // Теперь рендерим рамку вьюпорта для отладки.
+  //   //Теперь рендерим рамку вьюпорта для отладки.
   // final rectPaint = Paint()
-  //   ..color = Color(0xFFFF00FF) // Ярко-розовый цвет.
-  //   ..style = PaintingStyle.stroke // Рисуем только контур.
-  //   ..strokeWidth = 4.0; // Толщина линии.
+  //   ..color = Color(0xFFFF00FF) //Ярко-розовый цвет.
+  //   ..style = PaintingStyle.stroke //Рисуем только контур.
+  //   ..strokeWidth = 4.0; //Толщина линии.
   // final viewportRect = Rect.fromLTWH(
-  //   0, // X координата левого верхнего угла.
-  //   0, // Y координата левого верхнего угла.
-  //   camera.viewport.size.x, // Ширина рамки равна ширине вьюпорта.
-  //   camera.viewport.size.y, // Высота рамки равна высоте вьюпорта.
+  //   0, //X координата левого верхнего угла.
+  //   0, //Y координата левого верхнего угла.
+  //   camera.viewport.size.x, //Ширина рамки равна ширине вьюпорта.
+  //   camera.viewport.size.y, //Высота рамки равна высоте вьюпорта.
   // );
   // canvas.drawRect(viewportRect, rectPaint);
   // }
